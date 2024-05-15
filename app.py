662911c663e4d1d38bf65c03643c34374953f85a
Raniery Fialho <ranieryfialho@gmail.com>
2024-05-15 23:33:03 +0000

    att

--- app.py
+++ app.py
@@ -3,20 +3,20 @@ import pandas as pd
 
 def main():
     st.title("Primeira Versão")
-    st.write(carregarDados())  
+    df = carregarDados()
+    st.write(df)  # Exibe a tabela
+    st.subheader('Gráfico de Salário por Funcionários')
+    st.bar_chart(df.set_index('Nome')['Salário'])  # Exibe o gráfico
 
 def carregarDados():
-    # simulador de um carregamento de dados
+    # Simulador de um carregamento de dados
     data = {
         'Nome': ['Maria', 'José', 'Rafael', 'Raimunda'],
         'Idade': [24, 56, 34, 44],
         'Salário': [3000, 4000, 6000, 7000]
     }
     df = pd.DataFrame(data)
-    st.subheader('Gráfico de Salário por funcionários')
-    st.bar_chart(data, x='Nome', y='Salário') 
     return df
-
 
 if __name__ == "__main__":
     main()
