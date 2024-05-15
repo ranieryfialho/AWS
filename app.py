9d69e34dd1ab352ac16d3da5b461dc7c53b0e67b
Raniery Fialho <ranieryfialho@gmail.com>
2024-05-15 23:12:51 +0000

    att

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ import pandas as pd
 
 def main():
     st.title("Primeira Versão")
+    st.write(carregarDados())
+
+def carregarDados():
+    # simulador de um carregamento de dados
+    data = {
+        'Nome': ['Maria', 'José', 'Rafael', 'Raimunda'],
+        'Idade': [24, 56, 34, 44],
+        'Salário': [3000, 4000, 6000, 7000]
+    }
+    df = pd.DataFrame(data)
+    return df
 
 if __name__ == "__main__":
     main()
